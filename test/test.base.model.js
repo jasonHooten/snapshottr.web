930c583b81983564a978ac09930811569f1d5a81
Jason Hooten <jason.hooten@gmail.com>
2014-01-03 00:38:25 -0600

    added some scafolding stuff and unit tests

---
+++ test/test.base.model.js
@@ -0,0 +1,28 @@
+﻿var Model = require("../models/Base"),
+        chai = require('chai'),
+        expect = chai.expect,
+        dbMockup = {};
+
+
+var model;
+
+beforeEach(function() {
+    model = new Model(dbMockup);
+});
+
+describe("{Models}", function() {
+    it("should create a new model", function() {
+        expect(model.extend).to.exist;
+    });
+
+    it("should be extendable", function() {
+        var OtherTypeOfModel = model.extend({
+            myCustomModelMethod: function() {}
+        });
+
+        var model2 = new OtherTypeOfModel(dbMockup);
+
+        expect(model2.db).to.exist;
+        expect(model2.myCustomModelMethod).to.exist;
+    });
+});
