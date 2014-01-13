4b24ea7c9e304d96b05cbe8527e1c0012ab1fe56
Jason Hooten <jason.hooten@gmail.com>
2014-01-13 00:21:52 -0600

    trying to get emberjs working correctly, not there yet

---
+++ assets/linker/js/snapController.js
@@ -0,0 +1,47 @@
+﻿var socket = io.connect();
+
+socket.request('/snap',{},function(response) {
+    Snapshottr = Ember.Application.create();
+
+        //Routes
+
+        Snapshottr.Router.map(function() {
+                this.resource('snap', function() {
+                        this.resource('like', {path: ':_id'});
+                });
+        });
+
+        //Controllers
+        Snapshottr.SnapController = Ember.Controller.extend({
+                like: function(snap) {
+                        socket.request('/snap/like', {
+                                id: snap.get('id')
+                        }, updatelike);
+                }
+        });
+
+        //Views
+
+        //Models
+
+        Snapshottr.Store = DS.Store.extend({
+                revision: 12,
+                adapter: 'DS.FixtureAdapter'
+        });
+
+        Snapshottr.Snap = DS.Model.extend({
+                like: DS.attr('integer'),
+        });
+
+        Snapshottr.Snap.FIXTURES = response;
+});
+
+socket.on('message', function(response) {
+        updatelike(response.data);
+});
+
+// Methods
+var updatelike = function(response){
+        var snap = Snapshottr.Snap.find(response.id);
+        snap.set('like', response.like);
+};
